693c4c14a65f22959fa0cfaa598d6d67eb660e9f
Xander Van_Houtte <xander.van.houtte@student.howest.be>
2024-01-17 14:04:48 +0100

    test 3d met bestand

--- frontend/src/components/Box.js
+++ frontend/src/components/Box.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Box } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { useFrame, useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-export default function BoxComponent() {
-  const meshRef = React.useRef();
+export default function ModelComponent() {
+  const meshRef = useRef();
+  const gltf = useLoader(GLTFLoader, process.env.PUBLIC_URL + '/scene.gltf');
 
   useFrame(() => {
     if (meshRef.current) {
@@ -16,9 +17,7 @@ export default function BoxComponent() {
     <>
       <ambientLight intensity={0.5} />
       <directionalLight position={[0, 10, 0]} intensity={1.5} />
-      <Box args={[2, 2, 2]} ref={meshRef}>
-        <meshLambertMaterial attach="material" color="hotpink" />
-      </Box>
+      <primitive object={gltf.scene} ref={meshRef} />
     </>
   );
 }
